f22a12ef8556746e344dddc7c67bd87631ce2a61
borisKnox <58700497+borisKnox@users.noreply.github.com>
2019-12-17 06:32:58 +0300

    codepush function

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!!!</Text>
+      <Text style={styles.textColor1}>Changes you make will automatically reload.</Text>
+      <Text style={styles.textColor2}>Shake your phone to open the developer menu.</Text>
     </View>
   );
 }
@@ -16,4 +18,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textColor1:{
+    fontSize: 18,
+  },
+
+  textColor2:{
+    fontSize: 8,
+  },
+
 });
